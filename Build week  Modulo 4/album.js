13ad7392ad5dd493c8f170259b3cf00c5df48dfd
Jelena-J05 <135748656+Jelena-J05@users.noreply.github.com>
2023-10-12 00:58:25 +0200

    album js update

--- Build week  Modulo 4/album.js
+++ Build week  Modulo 4/album.js
@@ -20,31 +20,63 @@ async function loadAlbumData() {
 const albumTitleRef = document.querySelector('#album-title');
 const sectionCoverRef = document.querySelector('section#cover');
 const sectionTracklistRef = document.querySelector('section#tracklist');
-const sectionBackground=document.querySelector('#background-album');
+const sectionBackground = document.querySelector('#background-album');
 
 window.onload = async function () {
     const data = await loadAlbumData();
 
     if (data) {
 
-        albumTitleRef.innerHTML = data.title;
-        sectionBackground.innerHTML= /*html*/`
-        <img class="img-fluid" src="${data.cover}" alt="${data.artist.name}">
-        `;
+
+        sectionBackground.innerHTML = /*html*/`
+    <div class="d-flex flex-row my-4 ms-4">
+        <img class="img-fluid" src="${data.cover_medium}" alt="${data.artist.name}" style="width:200px; height:200px">
+        <div class="d-flex flex-column justify-content-end ms-3 fw-bold">
+            <span class="fs-6 mb-2" > ALBUM </span>
+            <span class="fs-1 mb-4">${data.title}</span>
+        <div>
+            <img src="${data.artist.picture_small}" class="rounded-circle artist-photo"alt="Immagine dell'artista" style="width:40px; height:40px">
+            <span>${data.artist.name}</span>
+            <span> • <span>
+            <span> Anno </span>
+            <span> • <span>
+            <span> x Brani </span>
+            <span class="text-muted"> Durata. </span>
+            </div>
+        </div>
+    </div>
+`;
+        //inserire l'anno dell'album e la durata dell'album
         // sectionCoverRef.innerHTML = /*html*/`
         //     <img class="img-fluid" src="${data.cover_small}" alt="${data.title}">
         // `;
 
         sectionTracklistRef.innerHTML = /*html*/`
-            <ol class="text-dark w-100">
+            <div class="container">
+                <div class="row text-muted border-bottom">
+                    <div class="col-6 mb-2"> # TITOLO </div>
+                    <div class="col-3 mb-2"> RIPRODUZIONI </div>
+                    <div class="col-3 mb-2 d-flex justify-content-end"> <i class="bi bi-clock"></i> 
+                    </div>
+                </div>
+            </div>
+            <div>
+            <ol class="text-dark list-group list-group-numbered">
                 ${data.tracks.data.map(song => /*html*/`
-                    <li class="text-dark d-flex justify-content-between align-items-center">
-                    <img class="img-fluid" src="${data.cover_small}" alt="${data.title}">
-                    <span>${song.title} </span>
-                    <span>${timeStampFromDuration(song.duration)}</span>
+                    <li class="text-dark align-items-center d-flex flex-row list-group-item border border-0">
+                    <div class="col-6 d-flex flex-column justify-content-start ms-2">
+                    ${song.title} 
+                    <span class="text-muted">${data.artist.name}</span>
+                    </div>
+                    <div class="col-3 d-flex justify-content-start"> </div>
+                    <div class="col-3 d-flex justify-content-end">${timeStampFromDuration(song.duration)}</div>
                     </li>
                 `).join('')}
             </ol>
+            
         `;
     }
 };
+
+
+// <img class="img-fluid" src="${data.cover_small}" alt="${data.title}"> 
